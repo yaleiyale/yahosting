ffbc66f65b278807d1b447731e554bc39732abdd
yaleiyale <55282569+yaleiyale@users.noreply.github.com>
2022-11-07 14:48:07 +0800

    upload from emo-uploader·Github

---
+++ obsidian/1667803687000gkayci.bat
@@ -0,0 +1,25 @@
+chcp 65001
+@echo off
+set /p gd=输入要清除历史提交信息的仓库目录的绝对路径:
+echo 待处理的路径：%gd%
+set /p gm=输入提交说明：
+pushd
+set /p gb=输入要清除的分支:
+pushd
+cd /d %gd%
+git checkout --orphan latest_branch
+git add -A
+git commit -am "%gm%"
+git branch -D %gb%
+git branch -m %gb%
+git push -f origin %gb%
+git pull
+echo "已清除全部的历史记录!"
+echo "查看新仓库信息："
+git log --pretty=oneline
+git branch -a
+git tag
+git ls-remote --tags
+pause
+popd
+exit
